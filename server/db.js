bb5771b07b4675b94b4e08dc302f9fcfbe428bcd
dp287517 <94993631+dp287517@users.noreply.github.com>
2025-09-18 20:35:36 +0200

    Delete server directory

--- server/db.js
+++
@@ -1,7 +0,0 @@
-import pg from 'pg';
-const { Pool } = pg;
-
-export const pool = new Pool({
-  connectionString: process.env.NEON_DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
--- server/middlewares/siteScope.js
+++
@@ -1,5 +0,0 @@
-export function siteScope(req, _res, next){
-  const site = req.user?.site || req.headers['x-site'] || 'Default';
-  req.site = String(site);
-  next();
-}
