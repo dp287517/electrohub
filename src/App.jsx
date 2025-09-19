24ec0c8afa802a67b0e6fa12f73b1a7a188a65ad
dp287517 <94993631+dp287517@users.noreply.github.com>
2025-09-19 20:43:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ export default function App() {
         <Route path="/lost-password" element={<LostPassword />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/app/atex" element={<ProtectedRoute><Atex /></ProtectedRoute>} />
+        <Route path="/app/loopcalc" element={<ProtectedRoute><LoopCalc /></ProtectedRoute>} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
