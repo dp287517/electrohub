136a55b045f8aaeb6748811e7092b8ad8c154fc8
dp287517 <94993631+dp287517@users.noreply.github.com>
2025-10-02 10:32:37 +0200

    Update Oibt.jsx

--- src/pages/Oibt.jsx
+++ src/pages/Oibt.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { api } from "../api";
+import { api } from "../lib/api.js";
 
 export default function Oibt() {
   const [projects, setProjects] = useState([]);
