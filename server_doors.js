993f0b5a4d2e5df2b20637d50ac9e161bd19ed76
dp287517 <94993631+dp287517@users.noreply.github.com>
2025-10-17 10:11:33 +0200

    Update server_doors.js

--- server_doors.js
+++ server_doors.js
@@ -94,6 +94,7 @@ app.use(
 );
 
 app.use(express.json({ limit: "16mb" }));
+app.use("/standard_fonts", express.static(PDF_STANDARD_FONTS));
 
 const PORT = Number(process.env.FIRE_DOORS_PORT || 3016);
 const HOST = process.env.FIRE_DOORS_HOST || "0.0.0.0";
